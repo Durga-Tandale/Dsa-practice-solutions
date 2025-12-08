ed7b2c39cb70aac7f592ed04b0aeb5333a5a871a
Durga Tandale <dtandale@manh.com>
2025-12-08 12:22:43 +0530

    Sum_of_All_Subarrays using prefix sum approach

--- Module1/Class1_Time_Complexity/Q01_Sum_Of_N_Natural_Numbers.java
+++ Module1/Class1_Time_Complexity/Q01_Sum_Of_N_Natural_Numbers.java
@@ -1,10 +1,5 @@
 package Module1.Class1_Time_Complexity;
 
 public class Q01_Sum_Of_N_Natural_Numbers {
-
-    public static void main(String[] args){
-        //initial push check in 
-        System.out.println("Sum of N natural numbers");
-    }
     
 }
