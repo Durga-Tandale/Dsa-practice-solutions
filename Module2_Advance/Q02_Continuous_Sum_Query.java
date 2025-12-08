6c1effccfdce0a0134cb21f7af123612c23dc80b
Durga Tandale <dtandale@manh.com>
2025-12-08 12:37:02 +0530

    Initial check in for module 2

---
+++ Module2_Advance/Q02_Continuous_Sum_Query.java
@@ -0,0 +1,5 @@
+package Module2_Advance;
+
+public class Q02_Continuous_Sum_Query {
+    
+}
---
+++ Module2_Advance/Q03_Merge_Sorted_Overlapping_Intervals_2.java
@@ -0,0 +1,5 @@
+package Module2_Advance;
+
+public class Q03_Merge_Sorted_Overlapping_Intervals_2 {
+    
+}
