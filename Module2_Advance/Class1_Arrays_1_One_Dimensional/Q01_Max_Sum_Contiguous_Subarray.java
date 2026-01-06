cd8788a64bd4ef045a194b35f47844752eb5d74c
Durga Tandale <dtandale@manh.com>
2026-01-06 12:29:56 +0530

    Binary Search problems

---
+++ Module2_Advance/Class1_Arrays_1_One_Dimensional/Q01_Max_Sum_Contiguous_Subarray.java
@@ -0,0 +1,19 @@
+package Module2_Advance.Class1_Arrays_1_One_Dimensional;
+
+// #Kadanes algo
+// Q- max sum from all subarrays
+
+// iterate the original array
+// initialize the max to -infi, curr to 0.
+// add values of array into currsum one by one
+// maintain, curr and max sum
+// compare evrytime curr,mx, whatever max is, add in maxsum
+// if -ve value comes in array, reset currsum to 0.
+// return the array maxsum at end.
+
+
+public class Q01_Max_Sum_Contiguous_Subarray {
+
+
+    
+}
---
+++ Module3_Advance/Q_Idx_of_element_K_In_Sorted_Array.java
@@ -0,0 +1,30 @@
+// Q - given sorted(distinct) array of size N, find the idx of element k, if k doesn't exist return -1.
+//k = 20
+package Module3_Advance;
+
+public class Q_Idx_of_element_K_In_Sorted_Array {
+
+public int bSearch(int[] A, int K){
+    int N = A.length;
+    int lo = 0, hi = N-1;
+    
+    while(lo<=hi){
+       int mid = lo + (hi-lo)/2;
+
+       if(A[mid] == K){
+        return mid;
+       }
+       else if(A[mid] < K){
+        //discard left
+        lo = mid+1;
+       }
+       else{
+        //discard right
+        hi = mid-1;
+       } 
+    }
+    return -1;
+}
+    
+}
+
